98e1eaee4aa49f9986fb247f7b6cad870dc1bab7
Martin Pechacek <mpechacek92@gmail.com>
2017-04-23 12:27:31 +0200

    Created project and basic form for adding new user

---
+++ Kucharka/Controllers/UsersController.cs
@@ -0,0 +1,51 @@
+﻿using Semestralka.DatabaseModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Semestralka.Controllers
+{
+    public class UsersController : Controller
+    {
+        //
+        // GET: /User/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /**
+         *  Method for inserting user account into database
+        **/
+        [HttpPost]
+        public ActionResult Index(User user)
+        {
+            using(KucharkaEntities kucharkaEntities = new KucharkaEntities())
+            {
+                //sends data into database
+                kucharkaEntities.Users.Add(user);
+                //save changes in database
+                kucharkaEntities.SaveChanges();
+                //initialization variable for error message
+                string message = string.Empty;
+
+                //storing information message into variable depending on value returned from databse procedure
+                switch (user.id_user)
+                {
+                    case -1:
+                        message = "Username already exists.\\nPlease choose a different username.";
+                        break;
+                    case -2:
+                        message = "Supplied email address has already been used.";
+                        break;
+                    default:
+                        message = "Registration successful.\\nUser Id: " + user.id_user.ToString();
+                        break;
+                }  
+            }
+            return View();
+        }
+	}
+}
---
+++ Kucharka/Models/AddUserModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Semestralka.Models
+{
+    public class AddUserModel
+    {
+        public int id_user { get; set; }
+        
+        [Required]
+        public string username { get; set; }
+
+        [Required]
+        public string password { get; set; }
+
+        [Required]
+        public string firstname { get; set; }
+
+        [Required]
+        public string lastname { get; set; }
+
+        [Required]
+        public bool user_right { get; set; }
+    }
+}
